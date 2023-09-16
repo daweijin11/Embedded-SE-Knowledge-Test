f1138dbf7719ebee6d2422cff3f2a5ea86e21fbc
daweijin11 <134002789+daweijin11@users.noreply.github.com>
2023-09-16 15:47:31 -0400

    Create PrintSquares.c

---
+++ PrintSquares.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+   int N,M;
+   printf("Please Enter N: ");
+   scanf("%d",&N);
+   printf("The Value of N is %d\n", N);
+   printf("Please Enter M: ");
+   scanf("%d",&M);
+   printf("The Value of M is %d\n", M);
+   void Square(int N, int M){
+        if (N <= 0 || M <= 0) {
+            printf("invalid value for N and M. N and Must be positive integers");
+            return 0;
+        }
+        while (N > 0 && M > 0) {
+            if(N > M) {
+                printf("%dx%d ",M,M);
+                N -= M;
+            }
+            if(N <= M) {
+                printf("%dx%d ",N,N);
+                M -= N;
+            }
+    }
+   }
+   Square(N,M);
+   return;
+}
